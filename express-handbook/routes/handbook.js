97aa1a89075dea4194da50a2c03dc5231bb4ed1a
jj-pa <krap9482@gmail.com>
2020-11-06 22:51:38 +0900

    [express-handbook] handbook CRUD API 추가

--- express-handbook/routes/handbook.js
+++ express-handbook/routes/handbook.js
@@ -1,17 +1,82 @@
-var express = require('express');
-var router = express.Router();
-var Handbook = require('../models').Handbook;
+const express = require('express');
+const router = express.Router();
+const Handbook = require('../models').Handbook;
+const sequelize = require('../models').sequelize;
 
-/* GET users listing. */
 router.get('/', function(req, res, next) {
   Handbook.findAll()
-    .then((users) => {
-      res.json(users);
+    .then((handbooks) => {
+      res.json(handbooks);
     })
     .catch((err) => {
       console.error(err);
       next(err);
     });
+});
+
+router.get('/:id', function(req, res, next) {
+  let handbookId  = req.params.id;
+
+  Handbook.findByPk(handbookId)
+    .then(function(handbook) {
+      res.json(handbook);
+    })
+    .catch((err) => {
+      console.error(err);
+      next(err);
+    });
+});
+
+router.post('/',async (req,res,next) => {
+  let body = req.body;
+
+  Handbook.create({
+    category: body.category,
+    title: body.title,
+    body: body.body,
+    erdat: sequelize.fn('NOW'),
+    aedat: sequelize.fn('NOW'),
+  })
+    .then(result => {
+      console.log("success");
+    })
+    .catch(err => {
+      console.log("fail");
+    })
+});
+
+router.put('/:id',async (req,res,next) => {
+  let body = req.body;
+  let handbookId  = req.params.id;
+
+  Handbook.update({
+    category: body.category,
+    title: body.title,
+    body: body.body,
+    aedat: sequelize.fn('NOW'),
+  },{
+    where: { id: handbookId }
+  })
+    .then(result => {
+      console.log("success");
+    })
+    .catch(err => {
+      console.log("fail");
+    })
+});
+
+router.delete('/:id',async (req,res,next) => {
+  let handbookId  = req.params.id
+
+  Handbook.destroy({
+    where: { id: handbookId }
+  })
+    .then(result => {
+      console.log("success");
+    })
+    .catch(err => {
+      console.log("fail");
+    })
 });
 
 module.exports = router;
